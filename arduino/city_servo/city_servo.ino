429c7fd8be8d59781a875bb45ec0431785a55dc4
edwardrf x200 <edwardrf@gmail.com>
2013-03-06 14:00:10 +0800

    Change to event loop Commands changes target position instead of acting on motor directly Reports its max range at startup. Added new commands    COMMANDS:    u##    go up ## steps where ## is an integer number. e.g. u100    d##    go down ## steps where ## is an integer number. e.g. d50    h      go home, back to 0 position    t      go top, to max position    m      go to mid point    p##    go to position ##, where ## is an integer number, e.g. p100    c      do calibration, go home, then top then home.    r      report range.

--- arduino/city_servo/city_servo.ino
+++ arduino/city_servo/city_servo.ino
@@ -8,8 +8,8 @@
  by Tom Igoe
  
  */
- 
- /*
+
+ /**
    Stepper motor control for City project
    First test with pushbuttons for moving a building up and down
    
@@ -17,6 +17,20 @@
    
    February, 2
    
+   Github
+   https://github.com/xinchejian/DaedalusCity
+   
+   Motor will do calibration at startup to calculate the 0 position, travel range.
+   
+   COMMANDS:
+   u##    go up ## steps where ## is an integer number. e.g. u100
+   d##    go down ## steps where ## is an integer number. e.g. d50
+   h      go home, back to 0 position
+   t      go top, to max position
+   m      go to mid point
+   p##    go to position ##, where ## is an integer number, e.g. p100
+   c      do calibration, go home, then top then home.
+   r      report range.
  */
 
 #include <Stepper.h>
@@ -28,12 +42,14 @@ const int buttonUp = 13; // set button pin for up  pin 13
 const int switchStart = 7;
 const int switchEnd = 6;
 const int LED = 13;
+const int reportInterval = 20; // report current position for every 20ms (50Hz), reduce useless traffic
 
 Stepper myStepper(stepsPerRevolution, 8,9,10,11);            
 
 long pos = 0;
 long max = 0;
-
+long target = 0;
+long reportTime = 0;
 void setup() {
   Serial.begin(115200);
   
@@ -59,57 +75,93 @@ void setup() {
   pinMode(switchStart, INPUT);
   pinMode(switchEnd, INPUT);
   
-  home();
+  // Init loop, go home to find zero, top to find out max range, then back to home again.
+  calibrate();
+  
+  // Report the range and current position.
+  reportRange();
+  report();
+  reportTime = millis();
 }
 
 void loop() {
-
   if (digitalRead(buttonUp)) {
-    up();
+    target = pos + 20;
   }else if (digitalRead(buttonDown)){
-    down();
+    target = pos - 20;
   }
   
   if(Serial.available() > 0) {
     // get incoming byte:
     char cmd = Serial.read();
+    int tmp = 0;
     switch(cmd){
       case 'u':
-        up();
+        tmp = Serial.parseInt();
+        target = pos + tmp;
         break;
       case 'd':
-        down();
+        tmp = Serial.parseInt();
+        target = pos - tmp;
         break;
       case 'h':
-        home();
+        target = 0;
         break;
       case 't':
-        top();
+        target = max;
         break;
       case 'm':
-        center();
+        target = max / 2;
+        break;
+      case 'p':
+        tmp = Serial.parseInt();
+        target = tmp;
+        break;
+      case 'c':
+        calibrate();
+        break;
+      case 'r':
+        reportRange();
         break;
     }
-    // Serial.println(pos); // comment by lumi
   }
-  //stop();
+  
+  // Move the motor towards the target pos by 1 step.
+  if(target > pos){
+    up();
+  }else if(target < pos){
+    down();
+  }
+  
+  // Send it home if it suppose to be at home
+  if(pos == 0 && digitalRead(switchStart) == 0) home();
+  
+  // Report current position for every reportInterval
+  // if now < reportTime, we had a overflow. Congrats, the setup is running for more than 50 days if this happens
+  long now = millis();
+  if(now - reportTime > reportInterval || now < reportTime){ 
+    report();
+    reportTime = millis();
+  }
+}
+
+void calibrate(){
+  home();
+  top();
+  home();
 }
 
 void up(){
-  int t = 10;
-  while(digitalRead(switchEnd) == 0 && t-- > 0){
+  while(digitalRead(switchEnd) == 0){
     myStepper.step(-1);
     pos ++;
-    serial(); // added by lumi
   }
 }
 
 void down(){
-  int t = 10;
-  while(digitalRead(switchStart) == 0 && t-- > 0) {
+  while(digitalRead(switchStart) == 0) {
     myStepper.step(1);
     pos --;
-    serial(); // added by lumi
   }
 }
 
@@ -118,10 +170,8 @@ void home(){
   while(digitalRead(switchStart) == 0){
     myStepper.step(1);
     pos--;
-    serial(); // added by lumi
   }
   pos = 0;
-  // serial(); // comment by lumi
 }
 
 void top(){
@@ -129,17 +179,8 @@ void top(){
   while(digitalRead(switchEnd) == 0){
     myStepper.step(-1);
     pos ++;
-    serial(); // added by lumi
   }
   max = pos;
-}
-
-// Got to the center position, WARNING: NO CHECKING
-void center(){
-  long middle = max / 2;
-  myStepper.step(pos - middle);
-  
-  pos = middle;
 }
 
 void stop(){
@@ -148,12 +189,15 @@ void stop(){
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
 }
-// for easy use. the serial function is here
-void serial(){
-  // serial commands for Touch Designer
-    Serial.print("Position=");
-    Serial.print(pos);
-    Serial.print('\n');
-    // serial end
+
+// Report position to controlling machine.
+void report(){
+  Serial.println(pos);
+}
+
+// Reports max range
+void reportRange(){
+  Serial.print("Range : ");
+  Serial.println(max);
 }
 
