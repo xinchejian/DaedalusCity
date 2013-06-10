336685de55cbc98b2cc3eea260675330a1540bdf
Lionello Lunesu <lio@lunesu.com>
2013-06-11 02:09:25 +0800

    Per track speed; optional fading

--- arduino/city_lights/city_lights.ino
+++ arduino/city_lights/city_lights.ino
@@ -37,76 +37,98 @@ void setup() {
 
 // The programs:
 
-const char* const Prog1[LightCount] = {
-  BLINK_SLOW,
-  BLINK_SLOW,
-  BLINK_SLOW,
-  FADE,
-  BLINK FADE "00000000000" FADE,
-  "001238763984569361401058123790612430679124360796079341212347129703406794123",
+struct Track
+{
+  int period;
+  const char* track;
 };
 
-const char* const Prog2[LightCount] = {
-  BLINK,
-  BLINK_SLOW,
-  ON OFF ON ON OFF OFF OFF ,
-  "4",
-  OFF,
-  FADE,
+struct Track Prog1[LightCount] = {
+  {100, BLINK_SLOW},
+  {100, BLINK_SLOW},
+  {100, BLINK_SLOW},
+  {100, FADE},
+  {100, BLINK FADE "00000000000" FADE},
+  {100, "001238763984569361401058123790612430679124360796079341212347129703406794123"},
 };
 
-const char* const Prog3[LightCount] = {
-  "00000000000000000000000000001000000000000000000000000000000000000000000000000000",
-  "00000000000000000000000000000100000000000000000000000000000000000000000000000000",
-  "00000000000000000000000000000010000000000000000000000000000000000000000000000000",
-  "00000000000000000000000000000001000000000000000000000000000000000000000000000000",
-  "00000000000000000000000000000000100000000000000000000000000000000000000000000000",
-  "00000000000000000000000000000000010000000000000000000000000000000000000000000000",
+struct Track Prog2[LightCount] = {
+  {100, BLINK},
+  {100, BLINK_SLOW},
+  {100, ON OFF ON ON OFF OFF OFF},
+  {100, "4"},
+  {100, OFF},
+  {100, FADE},
 };
 
-const char* const Prog4[LightCount] = {
-  ON,
-  ON,
-  ON,
-  ON,
-  ON,
-  ON,
+struct Track Prog3[LightCount] = {
+  {100, "00000000000000000000000000001000000000000000000000000000000000000000000000000000"},
+  {100, "00000000000000000000000000000100000000000000000000000000000000000000000000000000"},
+  {100, "00000000000000000000000000000010000000000000000000000000000000000000000000000000"},
+  {100, "00000000000000000000000000000001000000000000000000000000000000000000000000000000"},
+  {100, "00000000000000000000000000000000100000000000000000000000000000000000000000000000"},
+  {100, "00000000000000000000000000000000010000000000000000000000000000000000000000000000"},
+};
+
+struct Track Prog4[LightCount] = {
+  {100, ON},
+  {100, ON},
+  {100, ON},
+  {100, ON},
+  {100, ON},
+  {100, ON},
 };
 
 // Declare one program for each button
-const char* const* const Programs[ButtonCount] = { Prog1, Prog2, Prog3, Prog4 };
+struct Track * Programs[ButtonCount] = { Prog1, Prog2, Prog3, Prog4 };
 
 // State variables
-const char* const* program = Programs[0];
-int time = PeriodInterpolate * PeriodPWM;
-int prevLevel[LightCount] = {0,0,0,0,0,0};
-int lightLevel[LightCount] = {0,0,0,0,0,0};
+struct Track * program = Programs[0];
+int time = -1;
+int prevLevel[LightCount] = {0};
+int lightLevel[LightCount] = {0};
 int lightFrame[LightCount] = {-1,-1,-1,-1,-1,-1};
-int interpolate = 0;
-char buttonState[ButtonCount] = {0,0,0,0};
+char buttonState[ButtonCount] = {0};
+char fading[LightCount] = {0};
 
 // the loop routine runs over and over again forever:
 void loop() {  
   
   const int period = ++time % PeriodPWM;
-  const int interpolate = time / PeriodPWM;
 
-  if (interpolate == PeriodInterpolate)
-  {
-    time = 0;
-    for (int l = 0; l < LightCount; ++l) 
-    {
-      prevLevel[l] = lightLevel[l];
-      char ch = program[l][++lightFrame[l]];
-      if (ch == 0)
-        ch = program[l][lightFrame[l] = 0];
-      lightLevel[l] = Periods[ch - '0'];
-    }
-  }
-  
   for (int l = 0; l < LightCount; ++l)
   {
-    int ll = (prevLevel[l] * (PeriodInterpolate - interpolate) + lightLevel[l] * interpolate) / PeriodInterpolate;
+    const int periodInterpolate = program[l].period;
+
+    const int interpolate = (time / PeriodPWM) % periodInterpolate;
+    if (period == 0 && interpolate == 0)
+    {
+      const char * track = program[l].track;
+      prevLevel[l] = lightLevel[l];
+      char ch;
+      do {
+        ch = track[++lightFrame[l]];
+        switch (ch)
+        {
+        case '/':
+          fading[l] = 1;
+          continue;
+        case '-':
+          fading[l] = 0;
+          continue;
+        case 0:
+          lightFrame[l] = -1;
+          continue;
+        }
+      }
+      while (0);
+      lightLevel[l] = Periods[ch - '0'];
+    }
+    
+    // TODO: only interpolate in certain cases
+    int ll = lightLevel[l];
+    if (fading[l])
+      ll = (prevLevel[l] * (periodInterpolate - interpolate) + ll * interpolate) / periodInterpolate;
     //Serial.println(ll);
     digitalWrite(FirstLightPin + l, ll > period);
   }
@@ -122,8 +144,10 @@ void loop() {
   if (pressed >= 0)
   {
     program = Programs[pressed];
-    for (int l = 0; l < LightCount; ++l)
+    for (int l = 0; l < LightCount; ++l) {
       lightFrame[l] = -1;
+      fading[l] = 0;
+    }
   }
 
   //delay(1);
