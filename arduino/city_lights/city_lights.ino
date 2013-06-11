9ec8158aea894130fd9b1e5a5016b343ab597c54
Lionello Lunesu <lio@lunesu.com>
2013-06-11 14:53:14 +0800

    Optional interpolation; gamma corrected periods; power of 2 interpolation

--- arduino/city_lights/city_lights.ino
+++ arduino/city_lights/city_lights.ino
@@ -6,25 +6,8 @@ const int LightCount = 6;
 const int FirstButtonPin = 8;
 const int ButtonCount = 4;
 
-const int PeriodPWM = 16;
-const int PeriodInterpolate = 64;
-const int Periods[10] = {0, 1, 2, 3, 4, 5, 6, 8, 11, PeriodPWM};
-
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pins for the lights as outputs.
-  for (int led = 0; led < LightCount; ++led)
-  { 
-    pinMode(FirstLightPin + led, OUTPUT);
-  }    
-  // initialize the digital pins for the buttons as inputs
-  for (int but = 0; but < ButtonCount; ++but)
-  { 
-    pinMode(FirstButtonPin + but, INPUT_PULLUP);
-  }
-  // for debugging  
-  Serial.begin(9600);
-}
+#define PWMLOG 5
+const int Periods[10] = {0, 1, 2, 4, 6, 10, 14, 19, 25, 1 << PWMLOG};
 
 // Predefined patterns:
 #define ON "9"
@@ -39,114 +22,138 @@ void setup() {
 
 struct Track
 {
-  int period;
+  unsigned char period;
   const char* track;
 };
 
 struct Track Prog1[LightCount] = {
-  {100, BLINK_SLOW},
-  {100, BLINK_SLOW},
-  {100, BLINK_SLOW},
-  {100, FADE},
-  {100, BLINK FADE "00000000000" FADE},
-  {100, "001238763984569361401058123790612430679124360796079341212347129703406794123"},
+  {1, BLINK},
+  {2, BLINK},
+  {3, BLINK},
+  {4, BLINK},
+  {5, BLINK},
+  {6, BLINK},
 };
 
 struct Track Prog2[LightCount] = {
-  {100, BLINK},
-  {100, BLINK_SLOW},
-  {100, ON OFF ON ON OFF OFF OFF},
-  {100, "4"},
-  {100, OFF},
-  {100, FADE},
+  {3, "1"},
+  {3, "2"},
+  {3, "3"},
+  {3, "4"},
+  {7, "/09"},
+  {4, "/"FADE},
 };
 
 struct Track Prog3[LightCount] = {
-  {100, "00000000000000000000000000001000000000000000000000000000000000000000000000000000"},
-  {100, "00000000000000000000000000000100000000000000000000000000000000000000000000000000"},
-  {100, "00000000000000000000000000000010000000000000000000000000000000000000000000000000"},
-  {100, "00000000000000000000000000000001000000000000000000000000000000000000000000000000"},
-  {100, "00000000000000000000000000000000100000000000000000000000000000000000000000000000"},
-  {100, "00000000000000000000000000000000010000000000000000000000000000000000000000000000"},
+  {3, "00000000000000000000000000001000000000000000000000000000000000000000000000000000"},
+  {3, "00000000000000000000000000000100000000000000000000000000000000000000000000000000"},
+  {3, "00000000000000000000000000000010000000000000000000000000000000000000000000000000"},
+  {3, "00000000000000000000000000000001000000000000000000000000000000000000000000000000"},
+  {3, "00000000000000000000000000000000100000000000000000000000000000000000000000000000"},
+  {3, "00000000000000000000000000000000010000000000000000000000000000000000000000000000"},
 };
 
 struct Track Prog4[LightCount] = {
-  {100, ON},
-  {100, ON},
-  {100, ON},
-  {100, ON},
-  {100, ON},
-  {100, ON},
+  {3, ON},
+  {3, ON},
+  {3, ON},
+  {3, ON},
+  {3, ON},
+  {3, ON},
 };
 
 // Declare one program for each button
 struct Track * Programs[ButtonCount] = { Prog1, Prog2, Prog3, Prog4 };
 
 // State variables
-struct Track * program = Programs[0];
-int time = -1;
-int prevLevel[LightCount] = {0};
-int lightLevel[LightCount] = {0};
-int lightFrame[LightCount] = {-1,-1,-1,-1,-1,-1};
+struct Track * program;
+unsigned long time = ~0;
+
+struct Light {
+  char prev, level, fading;
+  unsigned int frame;
+};
+Light lights[LightCount] = {0};
+
 char buttonState[ButtonCount] = {0};
-char fading[LightCount] = {0};
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pins for the lights as outputs.
+  for (int led = 0; led < LightCount; ++led)
+  { 
+    pinMode(FirstLightPin + led, OUTPUT);
+  }    
+  // initialize the digital pins for the buttons as inputs
+  for (int but = 0; but < ButtonCount; ++but)
+  { 
+    pinMode(FirstButtonPin + but, INPUT_PULLUP);
+  }
+  program = Prog1;
+  // for debugging  
+  Serial.begin(9600);
+}
 
 // the loop routine runs over and over again forever:
 void loop() {  
   
-  const int period = ++time % PeriodPWM;
+  const int period = ++time & ((1 << PWMLOG) - 1);
 
   for (int l = 0; l < LightCount; ++l)
   {
-    const int periodInterpolate = program[l].period;
-
-    const int interpolate = (time / PeriodPWM) % periodInterpolate;
+    struct Light &light = lights[l];
+    struct Track &track = program[l];
+    
+    const int periodLog = track.period;
+    const int interpolate = (time >> PWMLOG) & ((1 << periodLog) - 1);
     if (period == 0 && interpolate == 0)
     {
       const char * track = program[l].track;
-      prevLevel[l] = lightLevel[l];
       char ch;
-      do {
-        ch = track[++lightFrame[l]];
+      while(1) 
+      {
+        ch = track[++light.frame];
         switch (ch)
         {
         case '/':
-          fading[l] = 1;
+          light.fading = 1;
           continue;
         case '-':
-          fading[l] = 0;
+          light.fading = 0;
           continue;
         case 0:
-          lightFrame[l] = -1;
+          light.frame = -1;
           continue;
         }
+        break;
       }
-      while (0);
-      lightLevel[l] = Periods[ch - '0'];
+      int p = Periods[ch - '0'];
+      light.prev = light.fading ? light.level : p;
+      light.level = p;
     }
     
-    // TODO: only interpolate in certain cases
-    int ll = lightLevel[l];
-    if (fading[l])
-      ll = (prevLevel[l] * (periodInterpolate - interpolate) + ll * interpolate) / periodInterpolate;
+    int ll = light.prev + (((light.level - light.prev) * interpolate) >> periodLog);
     //Serial.println(ll);
     digitalWrite(FirstLightPin + l, ll > period);
   }
 
-  int pressed = -1;
-  for (int but = 0; but < ButtonCount; ++but)
+  if (period == 0)
   {
-    char prev = buttonState[but];
-    char now = buttonState[but] = (digitalRead(FirstButtonPin + but) == HIGH);
-    if (!prev && now)
-      pressed = but;
-  }
-  if (pressed >= 0)
-  {
-    program = Programs[pressed];
-    for (int l = 0; l < LightCount; ++l) {
-      lightFrame[l] = -1;
-      fading[l] = 0;
+    int pressed = -1;
+    for (int but = 0; but < ButtonCount; ++but)
+    {
+      char prev = buttonState[but];
+      char now = buttonState[but] = (digitalRead(FirstButtonPin + but) == HIGH);
+      if (!prev && now)
+        pressed = but;
+    }
+    if (pressed >= 0)
+    {
+      program = Programs[pressed];
+      for (int l = 0; l < LightCount; ++l) {
+        lights[l].frame = -1;
+        lights[l].fading = 0;
+      }
     }
   }
 
